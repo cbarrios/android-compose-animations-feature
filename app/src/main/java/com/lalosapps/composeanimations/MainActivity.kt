6274bd1537e9dfb10a71ffc2365c5d334115e63e
cbarrios <lalobarrios16@yahoo.com>
2022-10-24 17:33:48 -0400

    master: (2) Animated Visibility

--- app/src/main/java/com/lalosapps/composeanimations/MainActivity.kt
+++ app/src/main/java/com/lalosapps/composeanimations/MainActivity.kt
@@ -3,6 +3,19 @@ package com.lalosapps.composeanimations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.lalosapps.composeanimations.ui.theme.ComposeAnimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +23,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAnimationsTheme {
-
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    var isVisible by remember { mutableStateOf(false) }
+                    Button(onClick = {
+                        isVisible = !isVisible
+                    }) {
+                        Text(text = "Toggle")
+                    }
+                    MyAnimatedVisibility(
+                        isVisible = isVisible,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun MyAnimatedVisibility(
+    isVisible: Boolean,
+    modifier: Modifier = Modifier
+) {
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = slideInHorizontally() + fadeIn(),
+        modifier = modifier
+    ) {
+        Box(modifier = Modifier.background(Color.Red))
     }
 }
